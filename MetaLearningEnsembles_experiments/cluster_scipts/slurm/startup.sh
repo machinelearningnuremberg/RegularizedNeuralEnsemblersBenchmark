63f15ab8e7c3b08b1954b9e617aa60f464b1860f
worstseed <maciejanowski@gmail.com>
2023-08-17 23:07:34 +0200

    add cluster scripts

---
+++ MetaLearningEnsembles_experiments/cluster_scipts/slurm/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/anaconda3/bin/activate MetaLearningEnsembles
+
+basedir=/work/dlclarge2/janowski-quicktune/MetaLearningEnsembles/MetaLearningEnsembles_experiments
+workdir=/work/dlclarge2/janowski-quicktune/MetaLearningEnsembles/MetaLearningEnsembles_experiments
+
+cd "$basedir"
+
+echo "base:""$basedir"
+echo "work:""$workdir"
