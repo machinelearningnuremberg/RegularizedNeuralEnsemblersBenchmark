c0465a02674a18ff71a5c5c60bbb7e7dad70d95b
sebastianpinedaar <sebastianpinedaarango@gmail.com>
2023-08-22 16:43:27 +0200

    changes in base_metadataset, quicktune metadataset

---
+++ SearchingOptimalEnsembles/tests/test_quicktune_metadataset.py
@@ -0,0 +1,9 @@
+import SearchingOptimalEnsembles.metadatasets.quicktune_metadataset as qmd
+import torch
+from base_test import test_evaluate_ensembles, test_get_batch
+
+def test_quicktune_metadataset():
+
+    metadataset = qmd.QuickTuneMetaDataset()
+    test_evaluate_ensembles(metadataset)
+    test_get_batch(metadataset)
