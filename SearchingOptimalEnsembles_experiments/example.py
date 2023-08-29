b8d3ebddce24a2c3916209015809c4a2c3858a1f
worstseed <maciejanowski@gmail.com>
2023-08-29 02:26:07 +0200

    Small changes

--- SearchingOptimalEnsembles_experiments/example.py
+++ SearchingOptimalEnsembles_experiments/example.py
@@ -4,4 +4,7 @@ import SearchingOptimalEnsembles as SOE
 
 logging.basicConfig(level=logging.DEBUG)
 
-SOE.run(metadataset_name="scikit-learn")
+# SOE.run(metadataset_name="scikit-learn", surrogate_name="dre")
+# SOE.run(metadataset_name="scikit-learn", surrogate_name="dkl")
+# SOE.run(metadataset_name="quicktune", surrogate_name="dre")
+SOE.run(metadataset_name="quicktune", surrogate_name="dkl")
