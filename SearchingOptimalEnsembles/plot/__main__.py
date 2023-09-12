6d70eb040b100cc7f5f7dc0372846a1727cfe453
worstseed <maciejanowski@gmail.com>
2023-09-12 14:46:16 +0200

    Add incumbent plotting

---
+++ SearchingOptimalEnsembles/plot/__main__.py
@@ -0,0 +1,119 @@
+""" Plot from wandb
+
+Usage:
+    python -m SearchingOptimalEnsembles.plot [--metadatasets] [--algorithms] [--x_range] [--log_x]
+    [--log_y] [--filename] [--extension] [--dpi] [--output_dir] [--plot_type] user_name project_name group_name
+
+Positional arguments:
+    user_name                   Wandb user name
+    project_name                Wandb project name
+    group_name                  Wandb group name
+
+Optional arguments:
+    -h, --help                  Show this help message and exit
+    --benchmarks                List of metadatasets to plot
+    --algorithms                List of algorithms to plot
+    --x_range                   Bound x-axis (e.g. 1 10)
+    --log_x                     If true, toggle logarithmic scale on the x-axis
+    --log_y                     If true, toggle logarithmic scale on the y-axis
+    --filename                  Filename
+    --extension                 Image format
+    --dpi                       Image resolution
+    --output_dir                Output directory
+    --plot_type                 Type of plot to generate
+
+
+Note:
+    We have to use the __main__.py construct due to the issues explained in
+    https://stackoverflow.com/questions/43393764/python-3-6-project-structure-leads-to-runtimewarning
+
+"""
+
+
+import argparse
+import logging
+from pathlib import Path
+
+from .plot import plot
+
+parser = argparse.ArgumentParser(
+    prog="python -m seo.plot", description="Plot incumbent from WandB"
+)
+parser.add_argument(
+    "user_name",
+    help="Wandb user name",
+    # default="relea-research",
+)
+parser.add_argument(
+    "project_name",
+    help="Wandb project name",
+    # default="SearchingOptimalEnsembles",
+)
+parser.add_argument(
+    "group_name",
+    help="Wandb group name",
+    # default="debug",
+)
+parser.add_argument(
+    "--metadatasets", nargs="+", help="List of metadatasets to plot", default=None
+)
+parser.add_argument(
+    "--algorithms", nargs="+", help="List of algorithms to plot", default=None
+)
+parser.add_argument(
+    "--x_range", nargs="+", type=float, help="Bound x-axis (e.g. 1 10)", default=None
+)
+parser.add_argument(
+    "--log_x", action="store_true", help="If true, toggle logarithmic scale on the x-axis"
+)
+parser.add_argument(
+    "--log_y", action="store_true", help="If true, toggle logarithmic scale on the y-axis"
+)
+parser.add_argument(
+    "--filename",
+    default="incumbent_trajectory",
+    help="Filename",
+)
+parser.add_argument(
+    "--extension",
+    default="png",
+    choices=["png", "pdf"],
+    help="Image format",
+)
+parser.add_argument(
+    "--dpi",
+    type=int,
+    default=100,
+    help="Image resolution",
+)
+parser.add_argument(
+    "--output_dir",
+    default="./saved_plots",
+    help="Output directory",
+)
+parser.add_argument(
+    "--plot_type",
+    default="incumbent",
+    choices=["incumbent"],
+    help="Type of plot to generate",
+)
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.WARN)
+if args.x_range is not None and len(args.x_range) == 2:
+    args.x_range = tuple(args.x_range)
+plot(
+    user_name=args.user_name,
+    project_name=args.project_name,
+    group_name=args.group_name,
+    metadatasets=args.metadatasets,
+    algorithms=args.algorithms,
+    x_range=args.x_range,
+    log_x=args.log_x,
+    log_y=args.log_y,
+    filename=args.filename,
+    extension=args.extension,
+    dpi=args.dpi,
+    output_dir=Path(args.output_dir),
+    plot_type=args.plot_type,
+)
---
+++ SearchingOptimalEnsembles/plot/plot.py
@@ -0,0 +1,225 @@
+from __future__ import annotations
+
+import os
+from collections import defaultdict
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import wandb
+
+BASE_FONT_SIZE = 14
+
+
+def fetch_results(
+    user_name: str,
+    project_name: str,
+    group_name: str,
+    metadatasets: list[str] | None = None,
+    algorithms: list[str] | None = None,
+):
+    api = wandb.Api()
+
+    # Dictionary to hold results
+    results: defaultdict = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+    # Fetch runs
+    runs = api.runs(
+        f"{user_name}/{project_name}",
+        filters={"$and": [{"group": group_name}, {"state": "finished"}]},
+    )
+
+    # Algorithm filter, algorithms are passed as searcher_name + surrogate_name, joined by "_"
+    # e.g. "bo_dkl", let's get all possible searchers and surrogates
+    if algorithms is not None:
+        searcher_names = set()
+        surrogate_names = set()
+        for algorithm in algorithms:
+            searcher_name, surrogate_name = algorithm.split("_")
+            searcher_names.add(searcher_name)
+            surrogate_names.add(surrogate_name)
+
+    # Iterate over runs
+    for run in runs:
+        # searcher_name, metadataset_name, surrogate_name = run.group.split("_")
+        metadataset_name = [
+            tag.split("=")[1] for tag in run.tags if "metadataset" in tag
+        ][0]
+        if metadatasets is not None and metadataset_name not in metadatasets:
+            continue
+        searcher_name = [tag.split("=")[1] for tag in run.tags if "searcher" in tag][0]
+        if algorithms is not None and searcher_name not in searcher_names:
+            continue
+        surrogate_name = [tag.split("=")[1] for tag in run.tags if "surrogate" in tag][0]
+        if algorithms is not None and surrogate_name not in surrogate_names:
+            continue
+        dataset_name = [tag.split("=")[1] for tag in run.tags if "dataset" in tag][0]
+        # seed = [tag.split("=")[1] for tag in run.tags if "seed" in tag][0]
+        meta_num_epochs = [
+            tag.split("=")[1] for tag in run.tags if "meta_num_epochs" in tag
+        ][0]
+        max_num_pipelines = [
+            tag.split("=")[1] for tag in run.tags if "max_num_pipelines" in tag
+        ][0]
+
+        history = run.history(
+            keys=["incumbent (norm)", "searcher_iteration"], pandas=False
+        )
+        if history:
+            key = (searcher_name, surrogate_name, meta_num_epochs, max_num_pipelines)
+            incumbent_values = [record["incumbent (norm)"] for record in history]
+            iteration_values = [record["searcher_iteration"] for record in history]
+
+            results[metadataset_name][dataset_name][key].append(
+                (iteration_values, incumbent_values)
+            )
+
+    return results
+
+
+def plot_incumbent(
+    results: defaultdict,
+    output_dir: Path,
+    x_range: tuple | None = None,
+    log_x: bool = False,
+    log_y: bool = False,
+    filename: str = "incumbent_trajectory",
+    extension: str = "png",
+    dpi: int = 100,
+) -> None:
+    # Plot results
+    for metadataset_name, metadataset_results in results.items():
+        # num_datasets = len(metadataset_results)
+        fig, axs = plt.subplots(4, 3, figsize=(18, 18))  # 4 rows, 3 columns
+        fig.suptitle(
+            f"Metadataset: {metadataset_name}", fontsize=BASE_FONT_SIZE + 4, y=0.92
+        )
+
+        # Reduce space between subplots and legend
+        plt.subplots_adjust(hspace=0.4, wspace=0.4, bottom=0.15)
+
+        # Hide empty subplots
+        for ax_idx in range(
+            len(metadataset_results), 12
+        ):  # 4 rows x 3 columns = 12 subplots
+            row, col = divmod(ax_idx, 3)
+            axs[row, col].set_visible(False)
+
+        handles, labels = None, None
+        for ax_idx, (dataset_name, dataset_results) in enumerate(
+            metadataset_results.items()
+        ):
+            row, col = divmod(ax_idx, 3)  # 3 columns
+            ax = axs[row, col]
+
+            for key in dataset_results.keys():
+                all_iterations = []
+                all_incumbents = []
+
+                for iteration_values, incumbent_values in dataset_results[key]:
+                    all_iterations.append(
+                        iteration_values
+                    )  # Now we append the list instead of extending it
+                    all_incumbents.append(incumbent_values)  # Same here
+
+                # Convert to a numpy array and average over the first axis (assumed to be over seeds)
+                all_incumbents_array = np.array(all_incumbents)
+                mean_incumbents = np.mean(all_incumbents_array, axis=0)
+
+                # Calculate standard deviation and standard error
+                std_dev_incumbents = np.std(all_incumbents_array, axis=0)
+                num_seeds = all_incumbents_array.shape[0]
+                std_err_incumbents = std_dev_incumbents / np.sqrt(num_seeds)
+
+                # Make sure all lists have the same length before plotting
+                unique_iterations = sorted(
+                    set(all_iterations[0])
+                )  # Assuming all lists of iterations are identical
+
+                # if len(unique_iterations) != len(mean_incumbents):
+                #     raise ValueError(f"Mismatched dimensions: {len(unique_iterations)} vs {len(mean_incumbents)}")
+
+                (_,) = ax.plot(unique_iterations, mean_incumbents, label=str(key))
+                ax.fill_between(
+                    unique_iterations,
+                    mean_incumbents - std_err_incumbents,
+                    mean_incumbents + std_err_incumbents,
+                    alpha=0.3,
+                )
+
+                # Set the x-axis range
+                if x_range is not None:
+                    ax.set_xlim(x_range)
+
+                # Set the x-axis to log scale
+                if log_x:
+                    ax.set_xscale("log")
+
+                # Set the y-axis to log scale
+                if log_y:
+                    ax.set_yscale("log")
+
+            if col == 0:
+                ax.set_ylabel("Incumbent", fontsize=BASE_FONT_SIZE)
+
+            if row == 1:
+                ax.set_xlabel("Iteration", fontsize=BASE_FONT_SIZE)
+
+            if ax_idx == 0:
+                handles, labels = ax.get_legend_handles_labels()
+
+            ax.set_title(f"Dataset: {dataset_name}", fontsize=BASE_FONT_SIZE)
+
+        # Common legend below the entire figure
+        # Create a new axes for the legend at the bottom of the figure
+        legend_ax = fig.add_axes([0.1, 0.45, 0.8, 0.05])  # [left, bottom, width, height]
+
+        # Add the legend to the new axes
+        legend_ax.legend(handles, labels, loc="center", ncol=4, fontsize=BASE_FONT_SIZE)
+
+        # Hide the axes
+        legend_ax.axis("off")
+
+        # Save to disk
+        file_path = os.path.join(output_dir, f"{filename}.{extension}")
+        plt.savefig(file_path, bbox_inches="tight", dpi=dpi)
+        print(f"Plot saved to {file_path}")
+
+        # Close the figure to free up memory
+        plt.close(fig)
+
+
+def plot(
+    user_name: str,
+    project_name: str,
+    group_name: str,
+    output_dir: Path,
+    metadatasets: list[str] | None = None,
+    algorithms: list[str] | None = None,
+    plot_type: str = "incumbent",
+    x_range: tuple | None = None,
+    log_x: bool = False,
+    log_y: bool = False,
+    filename: str = "incumbent_trajectory",
+    extension: str = "png",
+    dpi: int = 100,
+) -> None:
+    # Create output directory, using path
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # Fetch results
+    results = fetch_results(user_name, project_name, group_name, metadatasets, algorithms)
+
+    # Plot results§
+    if plot_type == "incumbent":
+        plot_incumbent(
+            results,
+            output_dir=output_dir,
+            x_range=x_range,
+            log_x=log_x,
+            log_y=log_y,
+            filename=filename,
+            extension=extension,
+            dpi=dpi,
+        )
+    else:
+        raise ValueError(f"Unknown plot type: {plot_type}")
