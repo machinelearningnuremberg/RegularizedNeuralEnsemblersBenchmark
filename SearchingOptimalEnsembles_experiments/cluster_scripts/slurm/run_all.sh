0814707241bd7d87929eb3723a6bc63ff1f0de35
worstseed <maciejanowski@gmail.com>
2023-09-13 14:57:43 +0200

    Change experiment scripts

--- SearchingOptimalEnsembles_experiments/cluster_scripts/slurm/run_all.sh
+++ SearchingOptimalEnsembles_experiments/cluster_scripts/slurm/run_all.sh
@@ -10,8 +10,8 @@ python "$basedir"cluster_scripts/slurm/slurm_helper.py \
     --timelimit 86400 \
     --startup "$basedir"cluster_scripts/slurm/startup.sh \
     --array_min 1 \
-    --array_max 10 \
+    --array_max 150 \
     --memory_per_job "30000mb" \
     -o "$workdir"LOGS/ \
     -l "$workdir"LOGS/ \
-    "$basedir"cluster_scripts/slurm/all_experiments.txt
+    "$basedir"cluster_scripts/slurm/ablate_DKL_1.txt
