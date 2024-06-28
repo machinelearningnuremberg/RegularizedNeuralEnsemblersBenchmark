neural ensembler: fix the masking

SearchingOptimalEnsembles/metadatasets/custom/custom_metadataset.py
@@ -11,6 +11,7 @@ class CustomMetaDataset(Evaluator):
     The evaluations are built after the creation of the object given a set of base pipelines.
     If the base pipelines are not given, they are randomly initialized
     """
+    
     metadataset_name = "custom"
 
     def __init__(
@@ -22,7 +23,9 @@ class CustomMetaDataset(Evaluator):
         meta_split_ids: tuple[tuple, tuple, tuple] = ((0, 1, 2), (3,), (4,)),
         data_version: str = None, #DatasetName
         task_type: str = "Supervised Classification",
-        device: str = 'cpu'
+        device: str = 'cpu',
+        num_base_pipelines: int = 20,
+        **kwargs
     ):
         self.data_dir = data_dir
         self.seed = seed
@@ -30,8 +33,8 @@ class CustomMetaDataset(Evaluator):
         self.metric_name = metric_name
         self.data_version = data_version
         self.task_type = task_type
-        self.default_num_base_pipelines = 20
-        self.base_pipelines = []
+        self.num_base_pipelines = num_base_pipelines
+        self.base_pipelines = None
         self.device = device
 
         if self.task_type == "Supervised Regression":
@@ -79,7 +82,8 @@ class CustomMetaDataset(Evaluator):
 
         if base_pipelines is None:
             #random pipelines
-            self.base_pipelines, self.hp_candidates = RandomPipelineSampler(self.default_num_base_pipelines,
+            if self.base_pipelines is None:
+                self.base_pipelines, self.hp_candidates = RandomPipelineSampler(self.num_base_pipelines,
                                                                         random_state=self.seed).sample()
 
         else:
@@ -92,7 +96,7 @@ class CustomMetaDataset(Evaluator):
         self.num_pipelines = len(self.base_pipelines)
         self.num_classes = len(np.unique(y_val))
         self.num_samples = len(y_val)
-        self.hp_indices = torch.FloatTensor(np.arange(self.num_pipelines))
+        self.hp_candidates_ids = torch.FloatTensor(np.arange(self.num_pipelines))
         self.hp_candidates = torch.FloatTensor(self.hp_candidates)
         self.dataset_names = []
 

SearchingOptimalEnsembles/posthoc/neural_ensembler.py
@@ -93,7 +93,6 @@ class NeuralEnsembler(BaseEnsembler):
         ne_use_mask: bool = True,
         ne_unique_weights_per_function: bool = False,
         ne_dropout_rate: float = 0.,
-
         **kwargs,
     ) -> None:
         super().__init__(metadataset=metadataset, device=device)
@@ -400,6 +399,10 @@ class NeuralEnsembler(BaseEnsembler):
             output_dim = base_functions_train.shape[-1]  # [NUMBER OF BASE FUNCTIONS]
         elif self.net_type == "ffn":
             NetClass = EFFNet
+            input_dim = X_train.shape[-1]
+            output_dim = base_functions_train.shape[-1]  # [NUMBER OF BASE FUNCTIONS]
+        elif self.net_type == "ffn2":
+            NetClass = EFFNet2
             input_dim = X_train.shape[-1]
             output_dim = base_functions_train.shape[-1]  # [NUMBER OF BASE FUNCTIONS]
         elif self.net_type == "simple":
@@ -876,6 +879,134 @@ class EFFNet(nn.Module):  # Sample as Sequence
 
         #num_classes changed
         batch_size, num_samples, num_classes, num_base_functions = w.shape
+        w = w.reshape(batch_size, num_samples, -1)
+        w_norm = torch.nn.functional.softmax(w, dim=-1)
+        w_norm = w_norm.reshape(
+            batch_size, num_samples, num_classes, num_base_functions
+        )
+        w_norm = w_norm[:, -num_query_samples:]
+
+        x = torch.multiply(base_functions, w_norm).sum(axis=-1)
+        # x.shape: [BATCH_SIZE, NUM_SAMPLES, NUM_CLASSES]
+        # w_norm.shape : [BATCH SIZE X NUMBER OF SAMPLES  X NUMBER OF CLASSES X NUMBER OF BASE FUNCTIONS]
+        return x, w_norm
+
+
+
+class EFFNet2(nn.Module):  # Sample as Sequence
+    def __init__(
+        self,
+        input_dim=1,
+        hidden_dim=128,
+        output_dim=1,
+        num_layers=2,
+        dropout_rate=0,
+        num_heads=1,
+        add_y=False,
+        mask_prob=0.5,
+        inner_batch_size=50,
+        unique_weight_per_base_function=True
+    ):
+        super().__init__()
+
+        # input = [BATCH SIZE X NUMBER OF SAMPLES X NUMBER OF BASE FUNCTIONS X NUMBER OF CLASSES]
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+        self.add_y = add_y
+        self.mask_prob = mask_prob
+        self.inner_batch_size = inner_batch_size
+        self.unique_weight_per_base_function = unique_weight_per_base_function
+
+        if self.add_y:
+            input_dim += 1
+
+        self.embedding = nn.Linear(input_dim, hidden_dim)
+
+        first_encoder_modules = [
+            nn.Sequential( nn.ReLU(), nn.Linear(hidden_dim, hidden_dim))
+            for _ in range(num_layers)
+        ]
+        first_encoder_modules.append(nn.ReLU())
+
+        self.first_encoder = nn.ModuleList(first_encoder_modules)
+        self.second_encoder = nn.Sequential(nn.Linear(hidden_dim, hidden_dim),nn.ReLU())
+
+        self.relu = nn.ReLU()
+        self.out_layer = nn.Linear(hidden_dim, output_dim)
+        self.dropout_rate = dropout_rate
+
+
+    def get_batched_weights(self, x, base_functions, y=None, X_context=None, y_context=None, mask_context=None):
+
+        batch_size, num_samples, num_classes, num_base_functions = x.shape
+
+        if self.add_y:
+            y = -1*torch.ones(batch_size, num_samples, num_classes, 1).to(x.device)
+            x = torch.cat([x,y], dim=-1)
+
+        batch_size, num_samples, num_classes, num_base_functions = x.shape
+        x = x.transpose(1, 2).reshape(-1, num_samples, num_base_functions)
+
+        # [(BATCH SIZE X NUMBER OF CLASSES) X NUMBER OF SAMPLES X NUMBER OF BASE FUNCTIONS]
+        x = self.embedding(x)
+        for encoder in self.first_encoder:
+            x = encoder(x)
+
+        #x = self.second_encoder(x, src_mask=mask_context)
+        x = x.transpose(0, 1).unsqueeze(0)  # use batch size here
+
+        return x
+
+    def forward(
+        self, x, base_functions, y=None, X_context=None, y_context=None, mask_context=None
+    ):
+        # X = [BATCH SIZE X NUMBER OF SAMPLES  X NUMBER OF CLASSES X NUMBER OF BASE FUNCTIONS]
+        batch_size, num_samples, num_classes, num_base_functions = x.shape
+        num_query_samples = x.shape[1]
+
+        if self.dropout_rate > 0 and self.training:
+            mask= (torch.rand(size=(num_base_functions,)) > self.dropout_rate).float().to(x.device)
+            for i, dim in enumerate([batch_size, num_samples, num_classes]):
+                mask = torch.repeat_interleave(
+                    mask.unsqueeze(i), dim, dim=i
+                )
+            x = (x*mask)/(1-self.dropout_rate)
+            base_functions = base_functions*mask
+        else:
+            mask = None
+
+        w = []
+        idx = np.arange(num_classes)
+        for i in range(0, num_classes, self.inner_batch_size):
+            range_idx = idx[range(i, min(i + self.inner_batch_size, num_classes))]
+            temp_w = self.get_batched_weights(x = x[:,:,range_idx],
+                                              base_functions = base_functions[:,:,range_idx],
+                                               y= y,
+                                               X_context=X_context[:,:,range_idx] if X_context is not None else None,
+                                               y_context=y_context,
+                                               mask_context=mask_context)
+            w.append(temp_w)
+
+        w = torch.cat(w, axis=2)
+
+        if self.unique_weight_per_base_function:
+            w = w.mean(axis=2)
+            w = self.second_encoder(w)
+            w = torch.repeat_interleave(
+                w.unsqueeze(2), num_classes, dim=2
+            )
+            
+        w = self.out_layer(w)
+        #w = self.dropout(w)
+
+        #num_classes changed
+        batch_size, num_samples, num_classes, num_base_functions = w.shape
+        
+        if mask is not None:
+            #inf_mask = torch.where(mask == 0, float('-inf'), mask)
+            #w *= inf_mask
+            w = w.masked_fill(mask == 0, -1e9)
         w = w.reshape(batch_size, num_samples, -1)
         w_norm = torch.nn.functional.softmax(w, dim=-1)
         w_norm = w_norm.reshape(