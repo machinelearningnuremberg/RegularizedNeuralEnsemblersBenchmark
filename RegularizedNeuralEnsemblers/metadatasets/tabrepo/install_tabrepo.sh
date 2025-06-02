ec9165ad128307ce664a808feb49f794ba65792a
worstseed <maciejanowski@gmail.com>
2025-06-02 14:12:55 +0200

    Add the installation scripts back

---
+++ RegularizedNeuralEnsemblers/metadatasets/tabrepo/install_tabrepo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the path where the tabrepo should be cloned
+TABREPO_DIR="RegularizedNeuralEnsemblers/metadatasets/tabrepo/tabrepo"
+
+# Check if the tabrepo directory already exists
+if [ ! -d "$TABREPO_DIR" ]; then
+    # Clone the repository if it does not exist
+    git clone https://github.com/autogluon/tabrepo.git $TABREPO_DIR
+fi
+
+# Install tabrepo in editable mode using pip
+pip install -e $TABREPO_DIR
+
+echo "tabrepo has been installed."
---
+++ RegularizedNeuralEnsemblers/posthoc/phem/install_phem.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Define the path to the phem submodule
+PHEM_DIR="RegularizedNeuralEnsemblers/posthoc/phem/phem"
+
+# Install phem in editable mode using pip
+pip install -e $PHEM_DIR 
+
+echo "phem has been installed."
